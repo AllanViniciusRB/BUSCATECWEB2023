a01c7aa6120f1dd257e6310862c16eb2f723a29f
Allan <allan.vralmeida@senacsp.edu.br>
2023-10-29 19:29:36 -0300

    buscatecweb

---
+++ includes/cabecalho.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600&display=swap" rel="stylesheet"/>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="/CSS/style.css">
+    <title>Busca tec</title>
+</head>
+    <!-- HEADER -->
+
+<body>
+
+    <div class="bg-home">
+        <header>
+            <nav class="header-content">
+
+                <!--Fundo-->
+                <div class="fundo">
+                    <img src="/assets/fundo.svg" width="100%" >
+                </div>
+
+                <!--Logo-->
+                <div class="logos">
+                    <img src="/assets/logo.svg">
+                </div>
+                <div class="user">
+                    <img src="/assets/user.png">
+                </div>
+                <!--botãos-->
+                <nav>
+                    <div class="links">  
+                        <ul class="nav-itens">
+                            <li>Planos</li>
+                            <li>Serviços</li>
+                            <li>Afiliar-se</li>
+                            <li>Entrar/Cadastre-se</li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="pesquisar">
+                    <div class="container">
+                        <div class="input-wrapper">
+                            <i class="fa fa-search"></i>
+                            <input />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+           
+        </header>
+
+    </div>
+</body>
+</html>
---
+++ includes/rodape.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="/CSS/rodape.css">
+
+</head>
+
+<body>
+    <footer>
+        <div class="medias-sociais">
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-youtube"></i>
+            <i class="fa-brands fa-github"></i>
+        </div>
+        <ul class="links">
+            <li><a href="#">Programação</a></li>
+            <li><a href="#">Programação</a></li>
+            <li><a href="#">Programação</a></li>
+            <li><a href="#">Programação</a></li>
+            <li><a href="#">Programação</a></li>
+        </ul>
+
+    </footer>
+</body>
+
+</html>
